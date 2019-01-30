903e9cc07c309c1c75dccc735fc1a377dd7b546b
Arek <areksablewski@gmail.com>
2019-01-30 22:21:03 +0100

    change google tslint rule, add component register function

--- src/mainViewModel.ts
+++ src/mainViewModel.ts
@@ -4,15 +4,36 @@ class Main {
     jsButtonText: KnockoutObservable<string>;
     jsButtonCommonText: KnockoutObservable<string>;
     tsButtonDate: string;
+    tsButton: TSButton;
+    componentButtonConfiguration: TSButton;
+    componentButtonConfiguration2: TSButton;
+    jsButton: JSButton;
 
     constructor () {
         this.text = ko.observable("Main text");
 
-        this.tsButtonText = new TSButton().text;
-        this.tsButtonDate = new TSButton().date;
+        this.tsButton = new TSButton({
+            text: "TSButton text"
+        });
+        this.tsButtonText = this.tsButton.text;
+        this.tsButtonDate = this.tsButton.date;
         
-        this.jsButtonText = new JSButton().text;
-        this.jsButtonCommonText = new JSButton().common.text;
+        this.jsButton = new JSButton();
+        this.jsButtonText = this.jsButton.text;
+        this.jsButtonCommonText = this.jsButton.common.text;
+
+        this.componentButtonConfiguration = new TSButton({
+            text: "aa"
+        });
+
+        this.componentButtonConfiguration2 = new TSButton({
+            text: "bb"
+        });
+
+        ko.components.register("mnbutton", {
+            viewModel: (params) => params.instance,
+            template: `<button data-bind="text: text"></button>`
+        });
     }
 }
 
--- src/ts-components/ts-button/ts-button.ts
+++ src/ts-components/ts-button/ts-button.ts
@@ -1,9 +1,14 @@
+interface ITSButton {
+    text: string;
+}
+
 class TSButton {
     text: KnockoutObservable<string>;
     date: string;
     
-    constructor () {
-        this.text = ko.observable("TSButton text");
+    constructor (params: ITSButton) {
+        this.text = ko.observable(params.text);
+
         this.date = moment().format("DD-MM-YYYY");
     }
 }
