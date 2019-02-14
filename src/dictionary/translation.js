6c31161ff2d13494c0af7bbf9b2a7664a0050a6d
Arek <areksablewski@gmail.com>
2019-02-14 21:15:18 +0100

    dictionary added

---
+++ src/dictionary/translation.js
@@ -0,0 +1,17 @@
+var MN = MN || {};
+MN.Dictionary = MN.Dictionary || {};
+
+MN.Dictionary.MicroAccounting = {
+    Action: {
+        Action1: "@(Action 1)",
+        Action2: "@(Action 2)",
+        Action3: "@(Action 3)",
+        Action4: "@(Action 4)",
+    },
+    Views: {
+        View1: "@(View1)",
+        View2: "@(View2)",
+        View3: "@(View3)",
+        View4: "@(View4)",
+    },
+}
